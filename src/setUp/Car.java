7527baecb88ce1887d48efb3066a44e7f7ca6fd9
kaabboo <kedirshafi2@gmail.com>
2022-01-01 14:56:46 -0800

    made car in master branch

---
+++ src/setUp/Car.java
@@ -0,0 +1,11 @@
+package setUp;
+
+public class Car {
+
+    //this si car class from master branch
+
+    //we will have conflict
+
+    //because there is different text in different branches for car
+
+}
